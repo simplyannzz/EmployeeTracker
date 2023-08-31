f5ba3016340ee946d6fd673fb7fc35320ce2a476
Annie <anniebui0110@gmil.com>
2023-08-30 21:29:48 -0500

    nodes

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO departments (, departments_name)
+VALUES
+("")
+
+
+
+
+INSERT INTO roles (job_title, salary, department_id)
+VALUES
+("")
+
+
+
+INSERT INTO roles (first_name, last_name, roles_id, mangagers_id)
+VALUES
+("")
