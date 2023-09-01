c92461cbe273104b9708a613717a52ad0bfd5261
Annie <anniebui0110@gmil.com>
2023-09-01 11:18:00 -0500

    modfied files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,43 @@
 INSERT INTO departments (, departments_name)
 VALUES
-("")
+('Executive Board'),
+('Marketing'),
+('Human Resources'),
+('Finance'),
+('Engineering'),
+('Information Technology'),
+('Customer Relations'),
+('Research and Development'),
+('Legal'),
+('Maintenance');
 
 
 
 
 INSERT INTO roles (job_title, salary, department_id)
 VALUES
-("")
+('Chief Executive Officer', 820000.00 1),
+('Marketing Manager', 116000.00, 2),
+('HR Director', 178000.00, 3),
+('Finance Head', 145000.00, 4),
+('Senior Engineer', 108000.00, 5),
+('IT Manager', 176000.00, 6),
+('Customer Relations Manager', 52000.00, 7),
+('Research and Development Manager ', 150000.00, 8),
+('Legal Manager', 91000.00, 9),
+('Maintenance Manager', 86000.00, 10);
 
 
 
 INSERT INTO roles (first_name, last_name, roles_id, mangagers_id)
 VALUES
-("")
+('Annie', 'Bui', 1, 1),
+('Kim', 'Nguyen', 2, 2),
+('Gogo', 'Holland', 3, 3),
+('Naro', 'Asano', 4, 4),
+('Saliena', 'Asano', 5, 5),
+('Edric', 'Mocha', 6, 6),
+('Aveline', 'Gonzalas', 7, 7),
+('Kat', 'Diaz', 8, 8),
+('Chicka', 'Vo', 9, 9),
+('Joanell', 'Leggett', 10, 10);
