e3249ca50f7b2e1d8796fe96a0af432d2eac9449
Annie <anniebui0110@gmil.com>
2023-09-04 12:51:20 -0500

    employee added

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ USE employeetracker_db;
 
 CREATE TABLE departments (
    id INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50) NOT NULL
+   name VARCHAR(30) NOT NULL
 );
 CREATE TABLE roles (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL,
     FOREIGN KEY(department_id) REFERENCES departments(id)
 ); 
 CREATE TABLE employee (
-id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 role_id INT,
 manager_id INT,
 FOREIGN KEY(manager_id) REFERENCES employee(id),
--- db/seeds.sql
+++ db/seeds.sql
@@ -11,3 +11,10 @@ VALUES
 ('Legal'),
 ('Maintenance');
 
+INSERT INTO roles(title, salary, department_id)
+VALUES 
+    ("HR person", 50000.00, 1),
+    ("IT Manager", 80000.50, 2),
+    ("IT specialist", 50000.00, 2);
+
+
