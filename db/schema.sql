f60cb05f5963e0a3c306288586f382bb1cc83b07
Annie <anniebui0110@gmil.com>
2023-09-05 14:02:02 -0500

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY(department_id) REFERENCES departments(id)
 ); 
 CREATE TABLE employee (
